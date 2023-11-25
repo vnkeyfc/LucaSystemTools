4fb6ed50521de41e8c480ed2d434a62740a4cbec
hvpexe <phuhuynh923@gmail.com>
2023-11-25 18:30:46 +0700

    update cz2

--- LucaSystemTools/LucaSystemTools/CZ2Parser.cs
+++ LucaSystemTools/LucaSystemTools/CZ2Parser.cs
@@ -8,41 +8,35 @@ using System.Text;
 using LucaSystem;
 using LucaSystemTools;
 
-namespace ProtImage
-{
+namespace ProtImage {
     /// <summary>
     /// 仅供参考未完成
     /// </summary>
-    public class CZ2Parser : CZParserBase
-    {
-        public IEnumerable<byte> Decompress(StructReader Reader)
-        {
+    public class CZ2Parser : CZParserBase {
+        public IEnumerable<byte> Decompress(StructReader Reader) {
             List<byte> output = new List<byte>();
             uint fileCount = Reader.ReadUInt32();
             Dictionary<int, uint> rawSizeList = new Dictionary<int, uint>();
             Dictionary<int, uint> compressedSizeList = new Dictionary<int, uint>();
-            for (int i = 0; i < fileCount; i++)
-            {
+            for (int i = 0; i < fileCount; i++) {
                 uint fileCompressedSize = Reader.ReadUInt32();
                 uint fileRawSize = Reader.ReadUInt32();
                 rawSizeList.Add(i, fileRawSize);
                 compressedSizeList.Add(i, fileCompressedSize);
             }
 
-            for (int i = 0; i < fileCount; i++)
-            {
+            for (int i = 0; i < fileCount; i++) {
                 List<UInt16> lmzBytes = new List<UInt16>();
                 int totalcount = (int)compressedSizeList[i];
-                for (int j = 0; j < totalcount/2; j++)
-                {
+                for (int j = 0; j < totalcount / 2; j++) {
                     lmzBytes.Add(Reader.ReadUInt16());
                 }
+                if (totalcount % 2 == 1)
+                    Reader.ReadByte();
                 //解压lzw
-                /*byte[] re = unlzw(lmzBytes.ToArray());
-                output.AddRange(re);*/
+
                 string str = Lzw2Util.Decompress(lmzBytes);
-                foreach (var c in str)
-                {
+                foreach (var c in str) {
                     output.Add((byte)c);
                 }
             }
@@ -50,8 +44,7 @@ namespace ProtImage
         }
 
 
-        private Bitmap Export(byte[] Texture)
-        {
+        private Bitmap Export(byte[] Texture) {
             StructReader Reader = new StructReader(new MemoryStream(Texture));
             CZ2Header Header = new CZ2Header();
             Reader.ReadStruct(ref Header);
@@ -69,8 +62,7 @@ namespace ProtImage
                 //表
                 Pixel32_BGRA[] ColorPanel = new Pixel32_BGRA[16];
                 Pixel32_BGRA Pixel = new Pixel32_BGRA();
-                for (int i = 0; i < ColorPanel.Length; i++)
-                {
+                for (int i = 0; i < ColorPanel.Length; i++) {
                     Reader.ReadStruct(ref Pixel);
                     ColorPanel[i] = Pixel;
                 }
@@ -80,62 +72,50 @@ namespace ProtImage
 
                 //解压后的像素
                 Queue<int> queue = new Queue<int>();
-                foreach (var b in bytes)
-                {
+                foreach (var b in bytes) {
                     int low4bit = b & 0x0F;
                     int high4bit = (b & 0xF0) >> 4;
                     queue.Enqueue(low4bit);
                     queue.Enqueue(high4bit);
                 }
 
-                for (int y = 0; y < Header.Heigth; y++)
-                {
-                    for (int x = 0; x < Header.Width; x++)
-                    {
+                for (int y = 0; y < Header.Heigth; y++) {
+                    for (int x = 0; x < Header.Width; x++) {
                         int index = queue.Dequeue();
                         Picture.SetPixel(x, y, Color.FromArgb(ColorPanel[index].A, ColorPanel[index].R, ColorPanel[index].G, ColorPanel[index].B));
                     }
                 }
-            }
-            else if (Header.Colorbits == 8)//8bit
-            {
+            } else if (Header.Colorbits == 8)//8bit
+              {
                 System.Diagnostics.Debug.WriteLine(8);
                 Pixel32_BGRA[] ColorPanel = new Pixel32_BGRA[256];
                 Pixel32_BGRA Pixel = new Pixel32_BGRA();
-                for (int i = 0; i < ColorPanel.Length; i++)
-                {
+                for (int i = 0; i < ColorPanel.Length; i++) {
                     Reader.ReadStruct(ref Pixel);
                     ColorPanel[i] = Pixel;
                 }
 
                 var bytes = Decompress(Reader);
                 Queue<int> queue = new Queue<int>();
-                foreach (var b in bytes)
-                {
+                foreach (var b in bytes) {
                     queue.Enqueue(b);
                 }
                 bool isError = false;
                 // var ie = bytes.GetEnumerator();
-                for (int y = 0; y < Header.Heigth; y++)
-                {
-                    for (int x = 0; x < Header.Width; x++)
-                    {
-                        try
-                        {
+                for (int y = 0; y < Header.Heigth; y++) {
+                    for (int x = 0; x < Header.Width; x++) {
+                        try {
                             int index = queue.Dequeue();
                             //int index = BitConverter.ToInt16(new byte[] { ie.Current, 0x00 }, 0);
                             Picture.SetPixel(x, y, Color.FromArgb(ColorPanel[index].A, ColorPanel[index].R, ColorPanel[index].G, ColorPanel[index].B));
 
-                        }
-                        catch
-                        {
+                        } catch {
                             Console.WriteLine(isError);
                             isError = true;
                             break;
                         }
                     }
-                    if (isError)
-                    {
+                    if (isError) {
                         Console.WriteLine(isError);
                         break;
                     }
@@ -170,8 +150,7 @@ namespace ProtImage
 
         //作者：Wetor
         //时间：2019.1.18
-        public void PngToCZ1(string outfile)
-        {
+        public void PngToCZ1(string outfile) {
             Bitmap Picture = new Bitmap(File.Open(outfile, FileMode.Open));
             StructWriter Writer = new StructWriter(File.Open(outfile + ".cz1", FileMode.Create));
             CZ1Header header;
@@ -188,18 +167,15 @@ namespace ProtImage
             Pixel.R = 255;
             Pixel.G = 255;
             Pixel.B = 255;
-            for (int k = 0; k < 256; k++)
-            {
+            for (int k = 0; k < 256; k++) {
                 Pixel.A = (byte)k;
                 Writer.WriteStruct(ref Pixel);
             }
 
             byte[] bytes = new byte[Picture.Height * Picture.Width];
             int i = 0;
-            for (int y = 0; y < Picture.Height; y++)
-            {
-                for (int x = 0; x < Picture.Width; x++)
-                {
+            for (int y = 0; y < Picture.Height; y++) {
+                for (int x = 0; x < Picture.Width; x++) {
                     bytes[i] = Picture.GetPixel(x, y).A;
                     i++;
                 }
@@ -210,8 +186,7 @@ namespace ProtImage
             //System.Diagnostics.Debug.WriteLine("{0} {1} {2}", file_num, listBytes.Count, 130554);
             List<int[]> out_list = new List<int[]>();
             Writer.Write(file_num);
-            for (int k = 0; k < file_num; k++)
-            {
+            for (int k = 0; k < file_num; k++) {
                 List<int> listBytes = new List<int>();
                 StringBuilder decompressed = new StringBuilder();
                 byte[] tmp_bytes = new byte[130554];
@@ -221,8 +196,7 @@ namespace ProtImage
                     Array.Copy(bytes, k * 130554, tmp_bytes, 0, 130554);
 
 
-                foreach (char kk in tmp_bytes)
-                {
+                foreach (char kk in tmp_bytes) {
                     decompressed.Append(kk);
                 }
                 listBytes = LzwUtil.Compress(decompressed.ToString());
@@ -242,12 +216,9 @@ namespace ProtImage
                     tmp_list.Insert(0, 0);
                     tmp_str = Decompress(tmp_list);
                 }*/
-                if (k == file_num - 1)
-                {
+                if (k == file_num - 1) {
                     Writer.Write(bytes.Length - k * 130554);
-                }
-                else
-                {
+                } else {
                     Writer.Write(130554);
                 }
                 //Writer.Write(0xFFFD);
@@ -262,10 +233,8 @@ namespace ProtImage
            // Writer.Write(output.ToArray());
             System.Diagnostics.Debug.WriteLine(output.Count);*/
 
-            for (int k = 0; k < out_list.Count; k++)
-            {
-                for (int kk = 0; kk < out_list[k].Length; kk++)
-                {
+            for (int k = 0; k < out_list.Count; k++) {
+                for (int kk = 0; kk < out_list[k].Length; kk++) {
                     Writer.Write((UInt16)out_list[k][kk]);
                 }
 
@@ -274,30 +243,26 @@ namespace ProtImage
 
 
         }
-   
-        public void CZ1ToPng(string infile)
-        {
+
+        public void CZ1ToPng(string infile) {
             BinaryReader br = new BinaryReader(File.Open(infile, FileMode.Open));
             Bitmap texture = Export(br.ReadBytes((int)br.BaseStream.Length));
             texture.Save(infile + ".png", ImageFormat.Png);
             br.Close();
         }
 
-        public override void FileExport(string path, string outpath = null)
-        {
+        public override void FileExport(string path, string outpath = null) {
             CZ1ToPng(path);
         }
 
-        public override void FileImport(string path, string outpath = null)
-        {
+        public override void FileImport(string path, string outpath = null) {
             PngToCZ1(path);
         }
     }
 
 
 
-    public struct CZ2Header
-    {
+    public struct CZ2Header {
         [FString(Length = 4)]
         public string Signature;
         public uint HeaderLength;
--- LucaSystemTools/LucaSystemTools/Utils/Lzw2Util.cs
+++ LucaSystemTools/LucaSystemTools/Utils/Lzw2Util.cs
@@ -4,47 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LucaSystem
-{
+namespace LucaSystem {
     //from http://rosettacode.org/wiki/LZW_compression#C.23
-    public class Lzw2Util
-    {
+    public class Lzw2Util {
 
-        public static string Decompress(List<UInt16> compressed)
-        {
+        public static string Decompress(List<UInt16> compressed) {
             // build the dictionary
+            int dictSize = 256;
             Dictionary<int, string> dictionary = new Dictionary<int, string>();
-            for (int i = 0; i < 512 + 2; i++)
-            {
+            for (int i = 0; i < dictSize; i++) {
                 dictionary.Add(i, ((char)i).ToString());
             }
-                
 
-            string w = dictionary[compressed[0]];
-            compressed.RemoveAt(0);
+            string characters = dictionary[compressed[0] / 2];
+            //compressed.RemoveAt(0);
             StringBuilder decompressed = new StringBuilder();
 
-            foreach (int k in compressed)
-            {
-                string entry = null;
-                if (dictionary.ContainsKey(k))
-                    entry = dictionary[k];
-                else if (k == dictionary.Count)
-                    entry = w + w[0];
+            foreach (UInt16 k in compressed) {
+                var code = k / 2;
+                string entry = dictionary.ContainsKey(code) ? dictionary[code] :
+                    characters + characters[0];
 
                 decompressed.Append(entry);
 
                 // new sequence; add it to the dictionary
-                dictionary.Add(dictionary.Count, w + entry[0]);
-                dictionary.Add(dictionary.Count, dictionary[dictionary.Count]);
-                w = entry;
+                dictionary.Add(dictSize++, characters + entry[0]);
+                characters = entry;
             }
 
             return decompressed.ToString();
         }
 
-        public static List<int> Compress(string uncompressed)
-        {
+        public static List<int> Compress(string uncompressed) {
             // build the dictionary
             Dictionary<string, int> dictionary = new Dictionary<string, int>();
             for (int i = 0; i < 256; i++)
@@ -52,15 +43,11 @@ namespace LucaSystem
             dictionary.Add(((char)256).ToString(), 0);
             string w = string.Empty;
             List<int> compressed = new List<int>();
-            foreach (char c in uncompressed)
-            {
+            foreach (char c in uncompressed) {
                 string wc = w + c;
-                if (dictionary.ContainsKey(wc))
-                {
+                if (dictionary.ContainsKey(wc)) {
                     w = wc;
-                }
-                else
-                {
+                } else {
                     // write w to output
                     compressed.Add(dictionary[w]);
                     // wc is a new sequence; add it to the dictionary
@@ -75,8 +62,7 @@ namespace LucaSystem
             return compressed;
         }
 
-        public static List<int> Compress(byte[] uncompressed)
-        {
+        public static List<int> Compress(byte[] uncompressed) {
             // build the dictionary
             // build the dictionary
             Dictionary<string, int> dictionary = new Dictionary<string, int>();
@@ -86,16 +72,12 @@ namespace LucaSystem
             string w = string.Empty;
             List<int> compressed = new List<int>();
 
-            foreach (byte c in uncompressed)
-            {
+            foreach (byte c in uncompressed) {
 
                 string wc = w + (c + 256).ToString();
-                if (dictionary.ContainsKey(wc))
-                {
+                if (dictionary.ContainsKey(wc)) {
                     w = wc;
-                }
-                else
-                {
+                } else {
                     // write w to output
                     compressed.Add(dictionary[w]);
                     // wc is a new sequence; add it to the dictionary
